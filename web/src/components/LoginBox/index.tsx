2ba1a2fbc478cbdb938f982b8d9a16ee35f416a0
Adla Gomes <adlagomes@gmail.com>
2021-11-02 22:12:13 -0300

    modified

--- web/src/components/LoginBox/index.tsx
+++ web/src/components/LoginBox/index.tsx
@@ -1,11 +1,16 @@
 import styles from "./style.module.scss";
 import { VscGithubInverted } from "react-icons/vsc";
+import { useContext } from "react";
+import { AuthContext } from "../../contexts/auth";
 
 export function LoginBox() {
+  const { signInUrl, user } = useContext(AuthContext);
+  console.log(user);
+
   return (
     <div className={styles.loginBoxWrapper}>
       <strong>Entre e compartilhe sua mensagem</strong>
-      <a href="#" className={styles.signInWithGithub}>
+      <a href={signInUrl} className={styles.signInWithGithub}>
         <VscGithubInverted size="24" />
         Entrar com Github
       </a>
