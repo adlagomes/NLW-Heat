845839288e28030e8c1b2d2ecaecf5d05bb6333b
Adla Gomes <adlagomes@gmail.com>
2021-11-02 23:11:28 -0300

    Update App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,17 @@
+import { useContext } from "react";
 import { LoginBox } from "./components/LoginBox";
 import { MessageList } from "./components/MessageList";
+import { SendMessageForm } from "./components/SendMessageForm";
+import { AuthContext } from "./contexts/auth";
 import styles from "./styles/App.module.scss";
 
 export function App() {
+  const { user } = useContext(AuthContext);
   return (
     <main className={styles.contentWrapper}>
       <MessageList />
-      <LoginBox/>
-   </main>
-  )
+      {!!user ? <SendMessageForm /> : <LoginBox />}
+      <LoginBox />
+    </main>
+  );
 }
